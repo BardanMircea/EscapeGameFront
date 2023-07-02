0c0fb20baf3f050da27db61e5b425b896a5135c6
BardanMircea <mircea.c.bardan@gmail.com>
2023-07-02 21:46:53 +0200

    refactored HomePageView and HomepageController; added lock image in InscriptionPageView

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import "./App.css";
 import Header from "./layouts/HeaderEtFooter/Header";
-// import ConnexionView from "./layouts/ConnexionPage/ConnexionPageView";
+import ConnexionView from "./layouts/ConnexionPage/ConnexionPageView";
 import Footer from "./layouts/HeaderEtFooter/Footer";
-// import InscriptionPageController from "./layouts/InscriptionPage/InscriptionPageController";
+import InscriptionPageController from "./layouts/InscriptionPage/InscriptionPageController";
 import HomePageController from "./layouts/HomePage/HomePageController";
 
 function App() {
   return (
     <>
-
       <Header />
-      <HomePageController/>
+      {/* <HomePageController/> */}
       {/* <ConnexionView /> */}
-      {/* <InscriptionPageController/> */}
+      <InscriptionPageController/>
       {/* <Footer /> */}
     </>
   );
--- src/layouts/HomePage/HomePageController.tsx
+++ src/layouts/HomePage/HomePageController.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import SalleModel from "../../models/SalleModel";
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from "@mui/material";
-import { Height } from "@mui/icons-material";
+import HomePageView from "./HomePageView";
 
 const HomePageController = () => {
 
@@ -28,44 +27,7 @@ const HomePageController = () => {
     }, [])
 
     return(
-        <>
-            <h1>Escape Game Page d'Accueil</h1>
-            <h2>Nos Escape Rooms vous attendent</h2>
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12}} >
-                {salles.map((salle) => (
-                    <Grid item xs={2} sm={4} md={4} key={salle._id} sx={{ display: 'flex', justifyContent: 'center'}}>
-                         <Card sx={{ maxWidth: 345, width: 345}} >
-                            <CardActionArea>
-                                <CardMedia
-                                component="img"
-                                height="140"
-                                image={salle.img ? salle.img : "https://escapetheroom.com/wp-content/uploads/2018/11/Escape-the-room-OG.jpg"}
-                                alt="escape room img"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                    {salle.nom}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary"
-                                        sx={{ overflow: "hidden", textOverflow: "ellipsis", display: "-webkit-box", WebkitLineClamp: 3, WebkitBoxOrient: "vertical", height:35}}
-                                    >
-                                        {salle.description}    
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Détails
-                                </Button>
-                                <Button size="small" color="primary">
-                                    Resérver
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        </>
+        <HomePageView salles={salles}/>
     )
 }
 
