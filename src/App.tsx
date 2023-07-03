7a81d86b311ad2a510be3c5a829e30793aae2ecd
BardanMircea <mircea.c.bardan@gmail.com>
2023-07-03 17:17:29 +0200

    added lock icon to ConnexionPageView

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,22 @@ import ConnexionView from "./layouts/ConnexionPage/ConnexionPageView";
 import Footer from "./layouts/HeaderEtFooter/Footer";
 import InscriptionPageController from "./layouts/InscriptionPage/InscriptionPageController";
 import HomePageController from "./layouts/HomePage/HomePageController";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+
   return (
     <>
       <Header/>
-
-          <div className="content">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePageController />} />
-          <Route path="/login" element={<ConnexionView />} />
-          <Route path="/signup" element={<InscriptionPageController />} />
-        </Routes> 
-      </Router>
-         </div>
+      <div className="content">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePageController />} />
+            <Route path="/login" element={<ConnexionView />} />
+            <Route path="/signup" element={<InscriptionPageController />} />
+          </Routes> 
+        </Router>
+      </div>
       <Footer/>
     </>
   );
