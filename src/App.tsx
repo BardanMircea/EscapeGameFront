56ab0b54dfe7f37f8f1c9ffff182b06a4d9a1d25
BardanMircea <mircea.c.bardan@gmail.com>
2023-07-03 12:00:05 +0200

    implemented React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import "./App.css";
-
 import Header from "./layouts/HeaderEtFooter/Header";
 import ConnexionView from "./layouts/ConnexionPage/ConnexionPageView";
 import Footer from "./layouts/HeaderEtFooter/Footer";
 import InscriptionPageController from "./layouts/InscriptionPage/InscriptionPageController";
 import HomePageController from "./layouts/HomePage/HomePageController";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+
   return (
     <>
-
-      <Header />
-      <HomePageController/>
-      {/* <ConnexionView /> */}
-      {/* <InscriptionPageController/> */}
-      {/* <Footer /> */}
+      <Header/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePageController />} />
+          <Route path="/login" element={<ConnexionView />} />
+          <Route path="/signup" element={<InscriptionPageController />} />
+        </Routes> 
+      </Router>
+      <Footer />
     </>
   );
 }
