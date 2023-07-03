c9eee56fba232245a9d935de3790db9492c4c5c3
BardanMircea <mircea.c.bardan@gmail.com>
2023-07-03 11:17:47 +0200

    savepoint

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import ConnexionView from "./layouts/ConnexionPage/ConnexionPageView";
 import Footer from "./layouts/HeaderEtFooter/Footer";
 import InscriptionPageController from "./layouts/InscriptionPage/InscriptionPageController";
 import HomePageController from "./layouts/HomePage/HomePageController";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
+
       <Header />
-      <HomePageController/>
-      {/* <ConnexionView /> */}
-      {/* <InscriptionPageController/> */}
-      {/* <Footer /> */}
+      <Routes>
+        <Route path="/" element={<HomePageController />} />
+        <Route path="/login" element={<ConnexionView />} />
+        <Route path="/signup" element={<InscriptionPageController />} />
+      </Routes>
+      
+      <Footer />
     </>
   );
 }
