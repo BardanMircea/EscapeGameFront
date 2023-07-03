ec11985781fbb65d21667589a1ae4b3bb4b6d6e3
PaulineHavez <131375628+PaulineHavez@users.noreply.github.com>
2023-07-03 11:13:32 +0200

    position footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 import Footer from "../components/Footer";
-// import Header from "../components/Footer";
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
       <Footer />
     </>
   );
