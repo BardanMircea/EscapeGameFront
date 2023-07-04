100ff05b9f60db2bab6e6febe4321f5c637c35e3
BardanMircea <mircea.c.bardan@gmail.com>
2023-07-04 23:36:44 +0200

    refactored SalleDetailsPage

--- src/layouts/SalleDetailsPage/SalleDetailsPageController.tsx
+++ src/layouts/SalleDetailsPage/SalleDetailsPageController.tsx
@@ -1,11 +1,11 @@
-import { Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button, ButtonGroup} from "@mui/material"
-import {Link, useParams} from "react-router-dom"
+import {useParams} from "react-router-dom"
 import SalleModel from "../../models/SalleModel"
 import JourModel from "../../models/JourModel"
 import { useEffect, useState } from "react"
+import SalleDetailsPageView from "./SalleDetailsPageView"
 
 const SalleDetailsPageController = () => {
-    // initialize a week variable
+    // initialize a week variable of type JourModel[]
     const semaine = [new JourModel("Lundi"), new JourModel("Mardi"), new JourModel("Mercredi"), new JourModel("Jeudi"), new JourModel("Vendredi"), new JourModel("Samedi"), new JourModel("Dimanche")]
 
     // state for salle and horaire
@@ -57,51 +57,7 @@ const SalleDetailsPageController = () => {
     }, [salle_id])
 
     return(
-        <>
-            <h1>{`${salle?.nom} Escape Room`}</h1>
-            <div className="card-details-container">
-            <Card sx={{width: 1000, maxWidth: 1200, maxHeight: 1000, mt:5, mb:5}} >
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="500"
-                    image={ salle?.img ? salle.img : "https://escapetheroom.com/wp-content/uploads/2018/11/Escape-the-room-OG.jpg"}
-                    alt="escape room img"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                        {salle?.nom}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary"
-                            sx={{ overflow: "hidden", textOverflow: "ellipsis", display: "-webkit-box", WebkitLineClamp: 100, WebkitBoxOrient: "vertical"}}
-                        >
-                            {salle?.description}
-                        </Typography>
-                    </CardContent>
-                    <h3>Disponibilité:</h3>
-                    <div className="week-container">
-                        
-                        {horaire.map((jour) => 
-                            <div className="day-container" key={jour.nom}>
-                                <h3>{jour.nom}</h3>
-                                <ButtonGroup orientation="vertical" variant="contained"aria-label="vertical contained button group">
-                                    {jour.disponibleMatin ? <Button style={{backgroundColor:"green"}}>Matin</Button> : <Button disabled>Matin</Button>}
-                                    {jour.disponibleAprem ? <Button style={{backgroundColor:"green"}}>Apres-midi</Button> : <Button disabled>Apres-midi</Button>}
-                                </ButtonGroup>
-                            </div>
-                        )}
-                    </div>
-                </CardActionArea>
-                <CardActions sx={{display:"flex", justifyContent:"center", mt:2}}>
-                    <Button component={Link} to="#" size="small" color="primary"  >
-                        Resérver
-                    </Button>
-                </CardActions>
-            </Card>
-        </div>
-    
-        </>
-        )
+        <SalleDetailsPageView salle={salle} horaire={horaire}/>
+    )
 }
-
 export default SalleDetailsPageController
