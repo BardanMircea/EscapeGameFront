15a6e064d4a21e10109ff1fcbe4205c68b6f7eb5
Donia <donia@aouali.net>
2023-07-07 12:09:06 +0200

    modification page admin

--- src/models/SalleModel.ts
+++ src/models/SalleModel.ts
@@ -1,17 +1,23 @@
 class SalleModel {
-    _id : number
-    nom : string
-    description : string
-    capacite : string
-    img? : string
+  _id: number;
+  nom: string;
+  description: string;
+  capacite: string;
+  img?: string;
 
-    constructor(id : number, nom : string, description : string, capacite : string, img? : string) {
-        this._id = id
-        this.nom = nom
-        this.description = description
-        this.capacite = capacite
-        if(img) this.img = img
-    }
+  constructor(
+    id: number,
+    nom: string,
+    description: string,
+    capacite: string,
+    img?: string
+  ) {
+    this._id = id;
+    this.nom = nom;
+    this.description = description;
+    this.capacite = capacite;
+    if (img) this.img = img;
+  }
 }
 
-export default SalleModel
+export default SalleModel;
